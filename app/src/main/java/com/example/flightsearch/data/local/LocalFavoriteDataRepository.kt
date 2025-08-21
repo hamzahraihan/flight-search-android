2fd5f190472f1a465b5ecca500b6e2c3679539f2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-21 16:27:46 +0700

    add local favorite class and initiate dao

---
+++ app/src/main/java/com/example/flightsearch/data/local/LocalFavoriteDataRepository.kt
@@ -0,0 +1,14 @@
+package com.example.flightsearch.data.local
+
+import com.example.flightsearch.model.Airport
+import com.example.flightsearch.model.Favorite
+import com.example.flightsearch.data.FavoriteDao
+import com.example.flightsearch.data.FavoriteRepository
+import kotlinx.coroutines.flow.Flow
+
+class LocalFavoriteDataRepository(private val favoriteDao: FavoriteDao) : FavoriteRepository {
+    override fun getAllFavorites(): Flow<List<Favorite>> = favoriteDao.getAllFavorites()
+
+    override suspend fun setFavoriteFlight(airport: Airport) =
+        favoriteDao.setFavoriteFlight(airport)
+}
