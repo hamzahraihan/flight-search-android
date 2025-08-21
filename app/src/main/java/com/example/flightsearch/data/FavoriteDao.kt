6062ce9ab491f4531f3580b46deed66bf12f38fd
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-21 16:24:48 +0700

    create data access object for favorite table

---
+++ app/src/main/java/com/example/flightsearch/data/FavoriteDao.kt
@@ -0,0 +1,22 @@
+package com.example.flightsearch.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.flightsearch.model.Airport
+import com.example.flightsearch.model.Favorite
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun setFavoriteFlight(airport: Airport)
+
+    @Query(
+        """
+        SELECT * from favorite ORDER BY destination_code ASC
+    """
+    )
+    fun getAllFavorites(): Flow<List<Favorite>>
+}
