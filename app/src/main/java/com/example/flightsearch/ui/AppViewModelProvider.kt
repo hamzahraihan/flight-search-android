e60d4ad031bd33f711533bc9c19cc120307efeb0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-21 16:23:53 +0700

    add view model provider

---
+++ app/src/main/java/com/example/flightsearch/ui/AppViewModelProvider.kt
@@ -0,0 +1,21 @@
+package com.example.flightsearch.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.flightsearch.FlightSearchApplication
+import com.example.flightsearch.ui.screens.AirportViewModel
+
+object AppViewModelProvider {
+    val Factory: ViewModelProvider.Factory = viewModelFactory {
+        initializer {
+            AirportViewModel(
+                airportRepository = flightSearchApplication().container.airportRepository,
+            )
+        }
+    }
+}
+
+fun CreationExtras.flightSearchApplication(): FlightSearchApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as FlightSearchApplication)
