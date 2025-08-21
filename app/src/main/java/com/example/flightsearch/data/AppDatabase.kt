417ce39f55442988bcce5a672688946e70057a25
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-21 16:23:37 +0700

    create app database for handling local database

---
+++ app/src/main/java/com/example/flightsearch/data/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.flightsearch.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.flightsearch.model.Airport
+import com.example.flightsearch.model.Favorite
+
+@Database(entities = [Airport::class, Favorite::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun airportDao(): AirportDao
+    abstract fun favoriteDao(): FavoriteDao
+
+    companion object {
+        @Volatile
+        private var Instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, AppDatabase::class.java, "flight_search.db")
+                    .createFromAsset("database/flight_search.db")
+                    .build().also { Instance = it }
+            }
+
+        }
+    }
+}
