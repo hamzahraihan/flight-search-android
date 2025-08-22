f570e8695aeb83418fa8f63981cd92bf4173553d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-22 16:02:28 +0700

    rename local data class

--- app/src/main/java/com/example/flightsearch/data/local/LocalAirportData.kt
+++ app/src/main/java/com/example/flightsearch/data/local/LocalAirportData.kt
@@ -5,7 +5,7 @@ import com.example.flightsearch.data.AirportDao
 import com.example.flightsearch.data.AirportRepository
 import kotlinx.coroutines.flow.Flow
 
-class LocalAirportDataRepository(private val airportDao: AirportDao) : AirportRepository {
+class LocalAirportData(private val airportDao: AirportDao) : AirportRepository {
     override fun getFlightBySearch(search: String): Flow<List<Airport>> =
         airportDao.getFlightBySearch(search)
 
