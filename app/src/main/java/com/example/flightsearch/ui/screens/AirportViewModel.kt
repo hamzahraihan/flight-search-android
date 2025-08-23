3cec370ff483c6dd75f4b18fe92f9904b57beb40
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-23 13:35:29 +0700

    remove getFlightBySearch and create getSearchSuggestions

--- app/src/main/java/com/example/flightsearch/ui/screens/AirportViewModel.kt
+++ app/src/main/java/com/example/flightsearch/ui/screens/AirportViewModel.kt
@@ -1,17 +1,29 @@
 package com.example.flightsearch.ui.screens
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.flightsearch.model.Airport
 import com.example.flightsearch.data.AirportRepository
+import com.example.flightsearch.data.UserPreferencesRepository
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
+@OptIn(FlowPreview::class)
 class AirportViewModel(
     private val airportRepository: AirportRepository,
+    private val userPreferencesRepository: UserPreferencesRepository
 ) : ViewModel() {
     private var _uiState = MutableStateFlow(AirportUiState())
 
@@ -19,22 +31,49 @@ class AirportViewModel(
     val uiState: StateFlow<AirportUiState> = _uiState.asStateFlow()
 
     init {
-        getFlightBySearch(input = uiState.value.searchInput)
+        viewModelScope.launch {
+            userPreferencesRepository.searchInput
+                .collectLatest { input ->
+                    if (_uiState.value.searchInput.isEmpty()) {
+                        _uiState.update { it.copy(searchInput = input) }
+                        getFlightBySearch(input = input)
+                    }
+                }
+        }
     }
 
-    fun getAllFlights() {
+//    fun getAllFlights() {
+//        viewModelScope.launch {
+//            airportRepository.getAllFlights().collect { fligths ->
+//                _uiState.update { it.copy(airportList = fligths) }
+//            }
+//        }
+//    }
+
+
+    fun updateUserInput(input: String) {
+        _uiState.update { it.copy(searchInput = input) }
+
+        getFlightBySearch(input)
+
         viewModelScope.launch {
-            airportRepository.getAllFlights().collect { fligths ->
-                _uiState.update { it.copy(airportList = fligths) }
+            userPreferencesRepository.saveSearchInput(input)
+        }
+    }
+
+    private fun getSearchAutocomplete(input: String) {
+        viewModelScope.launch {
+            airportRepository.getFlightBySearch("%$input%").filterNotNull().collect { airports ->
+                _uiState.update {
+                    Log.e("FROM DATABASE", airports.toString())
+                    it.copy(airportList = airports)
+                }
             }
         }
     }
 
-    fun getFlightBySearch(input: String) {
+    private fun getFlightBySearch(input: String) {
         viewModelScope.launch {
-            airportRepository.getFlightBySearch("%$input%").collect { flights ->
-                _uiState.update { it.copy(airportList = flights) }
-            }
         }
     }
 
@@ -48,16 +87,11 @@ class AirportViewModel(
 //            )
 
 
-    fun updateUserInput(input: String) {
-        _uiState.update { it.copy(searchInput = input) }
-    }
-
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
 }
 
 data class AirportUiState(
-    val searchInput: String = "",
-    val airportList: List<Airport> = emptyList()
+    val searchInput: String = "", val airportList: List<Airport> = emptyList()
 )
