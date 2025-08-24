923a1fde27fa944d39d2ba72e3d3d2245f3c4bb0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-24 15:54:57 +0700

    create favorites screen

---
+++ app/src/main/java/com/example/flightsearch/ui/screens/FavoriteScreen.kt
@@ -0,0 +1,13 @@
+package com.example.flightsearch.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FavoriteScreen(viewModel: FavoriteViewModel, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text("This is favorite screen")
+    }
+}
