f17ac15dc2f5b05f5dd1e9554414794b4e8877ba
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-24 15:55:30 +0700

    create view model for favorite flights
    This commit introduces `FavoriteViewModel` to manage and expose the
    favorite flights data to the UI. It includes:
    - `FavoriteUiState` data class to represent the UI state for favorite
    flights.
    - A `getFavoriteFlights` function to fetch favorite flights from the
    `FavoriteRepository` and update the UI state.
    - Initialization of the ViewModel to fetch favorite flights on creation.

---
+++ app/src/main/java/com/example/flightsearch/ui/screens/FavoriteViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.flightsearch.ui.screens
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flightsearch.data.FavoriteRepository
+import com.example.flightsearch.model.Airport
+import com.example.flightsearch.model.Favorite
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class FavoriteViewModel(private val favoriteRepository: FavoriteRepository) : ViewModel() {
+    private val _uiState = MutableStateFlow(FavoriteUiState())
+
+    val uiState: StateFlow<FavoriteUiState> = _uiState.asStateFlow()
+
+    init {
+        getFavoriteFlights()
+    }
+
+    fun getFavoriteFlights() {
+        viewModelScope.launch {
+            favoriteRepository.getAllFavorites().collect { favorites ->
+                _uiState.update {
+                    it.copy(favorites)
+                }
+            }
+        }
+    }
+}
+
+data class FavoriteUiState(
+    val favoriteAirport: List<Favorite> = emptyList()
+)
