a6aa3c986908effb156d387fc69222ffd98c2aa5
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-24 15:56:25 +0700

    create search suggestion screen
    This commit introduces a new screen, `SearchSuggestionScreen.kt`, which displays a list of suggested airports based on the user's search query.
    
    The screen consists of:
    - `SearchResultScreen`: The main composable function that observes the UI state from `AirportViewModel` and displays the `SearchList`.
    - `SearchList`: A `LazyColumn` that displays the suggested airports. Each item in the list shows the airport's IATA code and name. Clicking on an item updates the current airport in the `AirportViewModel`.

---
+++ app/src/main/java/com/example/flightsearch/ui/screens/SearchSuggestionScreen.kt
@@ -0,0 +1,59 @@
+package com.example.flightsearch.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchResultScreen(
+    viewModel: AirportViewModel,
+    modifier: Modifier = Modifier
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    Column(modifier = modifier) {
+        SearchList(
+            viewModel = viewModel,
+            uiState = uiState,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 18.dp)
+        )
+    }
+}
+
+@Composable
+private fun SearchList(
+    viewModel: AirportViewModel,
+    uiState: AirportUiState,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier) {
+        items(uiState.suggestedAirport) { airport ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp)
+                    .clickable {
+                        viewModel.updateCurrentAirport(airport.iataCode)
+                    },
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                Text(text = airport.iataCode, fontWeight = FontWeight.Bold)
+                Text(text = airport.name)
+            }
+        }
+    }
+}
