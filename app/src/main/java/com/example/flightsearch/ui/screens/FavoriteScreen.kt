b6d75a65839df0673f7b4bd63abf0281511fb66c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-25 14:04:13 +0700

    Add placeholder text in FavoriteScreen
    If there are no favorite flights, a placeholder text is shown.
    The placeholder includes a star icon and text.
    The placeholder is centered horizontally and vertically.

--- app/src/main/java/com/example/flightsearch/ui/screens/FavoriteScreen.kt
+++ app/src/main/java/com/example/flightsearch/ui/screens/FavoriteScreen.kt
@@ -1,13 +1,35 @@
 package com.example.flightsearch.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun FavoriteScreen(viewModel: FavoriteViewModel, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text("This is favorite screen")
+    val uiState by viewModel.uiState.collectAsState()
+
+    Column(modifier = modifier.fillMaxSize()) {
+        Text("Favorite Flights", fontWeight = FontWeight.Bold)
+        if (uiState.favoriteAirport.isEmpty()) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Icon(imageVector = Icons.Default.Star, contentDescription = null)
+                Text("You have no favorite flight", textAlign = TextAlign.Center)
+            }
+        }
     }
 }
