2d6488b3bbcbb0cb2baa1b4188c1996d10a631e3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-25 14:04:52 +0700

    enhance ui for suggested airport list
    The user can now tap on an airport to update the current airport and destination.
    The height of each item in the list is set to 40 dp for a more consistent look.
    The `currentAirport` is also logged for debugging purposes.

--- app/src/main/java/com/example/flightsearch/ui/screens/SearchSuggestionScreen.kt
+++ app/src/main/java/com/example/flightsearch/ui/screens/SearchSuggestionScreen.kt
@@ -1,10 +1,12 @@
 package com.example.flightsearch.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -44,12 +47,15 @@ private fun SearchList(
         items(uiState.suggestedAirport) { airport ->
             Row(
                 modifier = Modifier
+                    .height(40.dp)
                     .fillMaxWidth()
-                    .padding(bottom = 8.dp)
                     .clickable {
                         viewModel.updateCurrentAirport(airport.iataCode)
+                        viewModel.updateAirportByDestination(iataCode = airport.iataCode)
+                        Log.d("FROM CURRENT AIRPORT", uiState.currentAirport.toString())
                     },
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(text = airport.iataCode, fontWeight = FontWeight.Bold)
                 Text(text = airport.name)
