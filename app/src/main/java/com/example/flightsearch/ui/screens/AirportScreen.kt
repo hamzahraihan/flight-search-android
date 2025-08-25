93dd426e3eb93152f7c695c3e80f06e98c5503bd
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-25 14:03:55 +0700

    Create a basic screen to show airports

--- app/src/main/java/com/example/flightsearch/ui/screens/AirportScreen.kt
+++ app/src/main/java/com/example/flightsearch/ui/screens/AirportScreen.kt
@@ -1,13 +1,35 @@
 package com.example.flightsearch.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.room.util.convertByteToUUID
+import com.example.flightsearch.model.Airport
 
 @Composable
-fun AirportScreen(){
+fun AirportScreen(uiState: AirportUiState, modifier: Modifier) {
+    val airportList: List<Airport> = uiState.airportList
+    val currentAirport: Airport? = uiState.currentAirport
 
+    Column(modifier = modifier) {
+        Text("Flights from ${airportList.first().iataCode}")
+        AirportList(airportList = airportList, currentAirport)
+    }
 }
 
 @Composable
-fun AirportList(){
-
+fun AirportList(
+    airportList: List<Airport>,
+    currentAirport: Airport?,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier) {
+        items(airportList) {
+            Text(currentAirport!!.iataCode)
+            Text(it.iataCode)
+        }
+    }
 }
