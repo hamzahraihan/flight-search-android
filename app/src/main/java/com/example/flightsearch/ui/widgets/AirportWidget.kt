ee12340cfc47599d233f6970e1d575790314b5a4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-26 17:00:06 +0700

    create `AirportWidget.kt` to display airports

---
+++ app/src/main/java/com/example/flightsearch/ui/widgets/AirportWidget.kt
@@ -0,0 +1,121 @@
+package com.example.flightsearch.ui.widgets
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.flightsearch.model.Airport
+import com.example.flightsearch.model.Favorite
+
+@Composable
+fun AirportList(
+    airportList: List<Airport>,
+    isFavorite: (String, String) -> Boolean,
+    currentAirport: Airport?,
+    onClickFavorite: (Favorite) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier, verticalArrangement = Arrangement.spacedBy(12.dp)) {
+        items(airportList) { airport ->
+            AirportItem(
+                currentAirport = currentAirport!!,
+                isFavorite = isFavorite,
+                onClickFavorite = {
+                    onClickFavorite(it)
+                },
+                airport = airport,
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
+
+
+@Composable
+fun AirportItem(
+    currentAirport: Airport,
+    isFavorite: (String, String) -> Boolean,
+    airport: Airport,
+    onClickFavorite: (Favorite) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val isFavorite = isFavorite(currentAirport.iataCode, airport.iataCode)
+
+    Card(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                Text("Depart")
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(currentAirport.iataCode, fontWeight = FontWeight.Bold)
+                    Text(
+                        currentAirport.name,
+                        fontWeight = FontWeight.Light,
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                }
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text("Arrive")
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(airport.iataCode, fontWeight = FontWeight.Bold)
+                    Text(
+                        airport.name,
+                        fontWeight = FontWeight.Light,
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                }
+            }
+            IconButton(
+                onClick = {
+                    onClickFavorite(
+                        Favorite(
+                            departureCode = currentAirport.iataCode,
+                            destinationCode = airport.iataCode
+                        )
+                    )
+                },
+                content = {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Outlined.Star else Icons.Default.Star,
+                        contentDescription = null,
+                        tint = if (isFavorite) Color.Gray else Color.Yellow,
+                        modifier = Modifier
+                            .size(42.dp)
+                    )
+                }
+            )
+        }
+    }
+}
