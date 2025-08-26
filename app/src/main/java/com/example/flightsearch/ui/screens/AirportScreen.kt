d5a0528ae1c31cb20292a3d4fc853c6031045fc9
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-26 16:59:49 +0700

    implement flight list
    The `AirportScreen` now displays a list of flights from the currently selected airport. It utilizes the `AirportList` composable to render the flight information.
    
    The `isFavorite` and `onClickFavorite` parameters are added to `AirportScreen` to handle favorite flight interactions.
    
    The previous standalone `AirportList` composable has been integrated into the `AirportScreen`'s functionality, likely by moving its logic into the `ui.widgets.AirportList` composable.

--- app/src/main/java/com/example/flightsearch/ui/screens/AirportScreen.kt
+++ app/src/main/java/com/example/flightsearch/ui/screens/AirportScreen.kt
@@ -1,35 +1,39 @@
 package com.example.flightsearch.ui.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.room.util.convertByteToUUID
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.flightsearch.model.Airport
+import com.example.flightsearch.model.Favorite
+import com.example.flightsearch.ui.widgets.AirportList
 
 @Composable
-fun AirportScreen(uiState: AirportUiState, modifier: Modifier) {
+fun AirportScreen(
+    uiState: AirportUiState,
+    isFavorite: (String, String) -> Boolean,
+    onClickFavorite: (Favorite) -> Unit,
+    modifier: Modifier
+) {
     val airportList: List<Airport> = uiState.airportList
     val currentAirport: Airport? = uiState.currentAirport
 
     Column(modifier = modifier) {
-        Text("Flights from ${airportList.first().iataCode}")
-        AirportList(airportList = airportList, currentAirport)
-    }
-}
-
-@Composable
-fun AirportList(
-    airportList: List<Airport>,
-    currentAirport: Airport?,
-    modifier: Modifier = Modifier
-) {
-    LazyColumn(modifier = modifier) {
-        items(airportList) {
-            Text(currentAirport!!.iataCode)
-            Text(it.iataCode)
-        }
+        Text(
+            "Flights from ${currentAirport!!.iataCode}",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 12.dp)
+        )
+        AirportList(
+            airportList = airportList,
+            isFavorite = isFavorite,
+            onClickFavorite = {
+                onClickFavorite(it)
+            },
+            currentAirport = currentAirport
+        )
     }
 }
