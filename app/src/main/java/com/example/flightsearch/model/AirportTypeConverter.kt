2b21699e70be2d1b24f0ca1b8221ba164847662f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-27 10:15:18 +0700

    Refactor: rename AirportTypeConverter to FavoriteWithAirports
    The `AirportTypeConverter.kt` file has been renamed to `FavoriteWithAirports.kt`.
    
    The `FavoriteWithAirports` data class is introduced to represent a favorite flight along with its associated departure and destination airports. It uses Room's `@Embedded` and `@Relation` annotations to establish relationships between the `Favorite` and `Airport` entities.

--- app/src/main/java/com/example/flightsearch/model/AirportTypeConverter.kt
+++
@@ -1,7 +0,0 @@
-package com.example.flightsearch.model
-
-import androidx.room.TypeConverter
-
-
-class AirportTypeConverter {
-}
---
+++ app/src/main/java/com/example/flightsearch/model/FavoriteWithAirports.kt
@@ -0,0 +1,21 @@
+package com.example.flightsearch.model
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+
+data class FavoriteWithAirports(
+    @Embedded val favorite: Favorite,
+
+    @Relation(
+        parentColumn = "departure_code",
+        entityColumn = "iata_code"
+    )
+    val departureAirport: Airport?,
+
+    @Relation(
+        parentColumn = "departure_code",
+        entityColumn = "iata_code"
+    )
+    val destinationAirport: Airport?
+)
