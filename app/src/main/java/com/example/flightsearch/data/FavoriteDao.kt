df5026b78d54aaa849c4068f3e9863d75cb3ef8d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-27 10:14:49 +0700

    Implement Dao for Favorite table
    This commit adds CRUD operations for the `Favorite` table:
    - `getAllFavoriteWithAirports()`: Retrieves all favorite flights along with their departure and arrival airport details. This uses `@Transaction` to ensure atomicity.
    - `setFavoriteFlight()`: Inserts a new favorite flight, ignoring conflicts.
    - `deleteFavorite()`: Deletes a favorite flight.
    - `getAllFavorites()`: Retrieves all favorite flights ordered by destination code.
    - `exists()`: Checks if a specific favorite flight (based on departure and destination codes) already exists.

--- app/src/main/java/com/example/flightsearch/data/FavoriteDao.kt
+++ app/src/main/java/com/example/flightsearch/data/FavoriteDao.kt
@@ -1,17 +1,31 @@
 package com.example.flightsearch.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import com.example.flightsearch.model.Airport
 import com.example.flightsearch.model.Favorite
+import com.example.flightsearch.model.FavoriteWithAirports
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavoriteDao {
+    @Transaction
+    @Query(
+        """
+        SELECT * FROM favorite
+    """
+    )
+    fun getAllFavoriteWithAirports(): Flow<List<FavoriteWithAirports>>
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun setFavoriteFlight(favorite: Favorite)
+
+    @Delete
+    suspend fun deleteFavorite(favorite: Favorite)
 
     @Query(
         """
@@ -19,4 +33,11 @@ interface FavoriteDao {
     """
     )
     fun getAllFavorites(): Flow<List<Favorite>>
+
+    @Query(
+        """
+        SELECT COUNT(*) FROM favorite WHERE destination_code = :destinationCode AND departure_code = :departureCode
+    """
+    )
+    suspend fun exists(departureCode: String, destinationCode: String): Int
 }
