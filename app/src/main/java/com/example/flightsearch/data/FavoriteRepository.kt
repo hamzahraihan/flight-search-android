d4d85fd1572316fe5d15a7fe3d2c19c503798cc9
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-27 10:14:59 +0700

    add functions to favorite repository
    This commit adds the following functions to the `FavoriteRepository` interface:
    - `getAllFavoriteWithAirports()`: retrieves all favorite flights along with their airport details.
    - `deleteFavoriteFlight()`: deletes a specific favorite flight.
    - `exists()`: checks if a favorite flight with the given departure and destination codes already exists.

--- app/src/main/java/com/example/flightsearch/data/FavoriteRepository.kt
+++ app/src/main/java/com/example/flightsearch/data/FavoriteRepository.kt
@@ -2,9 +2,17 @@ package com.example.flightsearch.data
 
 import com.example.flightsearch.model.Airport
 import com.example.flightsearch.model.Favorite
+import com.example.flightsearch.model.FavoriteWithAirports
 import kotlinx.coroutines.flow.Flow
 
 interface FavoriteRepository {
     fun getAllFavorites(): Flow<List<Favorite>>
+
+    fun getAllFavoriteWithAirports(): Flow<List<FavoriteWithAirports>>
+
     suspend fun setFavoriteFlight(favorite: Favorite)
+
+    suspend fun deleteFavoriteFlight(favorite: Favorite)
+
+    suspend fun exists(departureCode: String, destinationCode: String): Int
 }
