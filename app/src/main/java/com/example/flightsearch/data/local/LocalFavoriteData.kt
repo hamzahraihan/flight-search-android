0c8d8767378d0a7b2aab7fa9cc1f0faba4e93e28
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-27 10:15:43 +0700

    add functions to favorite repository

--- app/src/main/java/com/example/flightsearch/data/local/LocalFavoriteData.kt
+++ app/src/main/java/com/example/flightsearch/data/local/LocalFavoriteData.kt
@@ -1,14 +1,23 @@
 package com.example.flightsearch.data.local
 
-import com.example.flightsearch.model.Airport
 import com.example.flightsearch.model.Favorite
 import com.example.flightsearch.data.FavoriteDao
 import com.example.flightsearch.data.FavoriteRepository
+import com.example.flightsearch.model.FavoriteWithAirports
 import kotlinx.coroutines.flow.Flow
 
 class LocalFavoriteData(private val favoriteDao: FavoriteDao) : FavoriteRepository {
     override fun getAllFavorites(): Flow<List<Favorite>> = favoriteDao.getAllFavorites()
 
+    override fun getAllFavoriteWithAirports(): Flow<List<FavoriteWithAirports>> =
+        favoriteDao.getAllFavoriteWithAirports()
+
     override suspend fun setFavoriteFlight(favorite: Favorite) =
         favoriteDao.setFavoriteFlight(favorite = favorite)
+
+    override suspend fun deleteFavoriteFlight(favorite: Favorite) =
+        favoriteDao.deleteFavorite(favorite)
+
+    override suspend fun exists(departureCode: String, destinationCode: String): Int =
+        favoriteDao.exists(departureCode, destinationCode)
 }
