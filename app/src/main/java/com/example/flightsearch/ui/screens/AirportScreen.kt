04a21eeeec207151247dc0f02db25d2297606d0c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-08-28 13:32:49 +0700

    Create LazyColumn to display list of airports
    The `AirportList` composable function was created to display a list of airports using `LazyColumn`.
    Each item in the list is an `AirportItem` composable.
    The `isFavorite` and `onClickFavorite` parameters are used to manage the favorite status of each airport.

--- app/src/main/java/com/example/flightsearch/ui/screens/AirportScreen.kt
+++ app/src/main/java/com/example/flightsearch/ui/screens/AirportScreen.kt
@@ -1,7 +1,11 @@
 package com.example.flightsearch.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,7 +13,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.flightsearch.model.Airport
 import com.example.flightsearch.model.Favorite
-import com.example.flightsearch.ui.widgets.AirportList
+import com.example.flightsearch.ui.widgets.AirportItem
 
 @Composable
 fun AirportScreen(
@@ -35,5 +39,28 @@ fun AirportScreen(
             },
             currentAirport = currentAirport
         )
+    }
+}
+
+@Composable
+fun AirportList(
+    airportList: List<Airport>,
+    isFavorite: (String, String) -> Boolean,
+    currentAirport: Airport,
+    onClickFavorite: (Favorite) -> Unit?,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier, verticalArrangement = Arrangement.spacedBy(12.dp)) {
+        items(airportList) { airport ->
+            AirportItem(
+                departureAirport = currentAirport,
+                destinationAirport = airport,
+                isFavorite = isFavorite,
+                onClickFavorite = {
+                    onClickFavorite(it)
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
     }
 }
